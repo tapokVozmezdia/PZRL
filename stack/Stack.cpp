fe0f53c443decfa3ff02b477bc2241bcbd8d337f
tapokVozmezdia <maxyRot@yandex.ru>
2023-05-18 21:08:27 +0300

    again..

--- stack/Stack.cpp
+++ stack/Stack.cpp
@@ -126,7 +126,8 @@ Stack& Stack::operator=(const Stack& copyStack)
             throw std::runtime_error("Неизвестный тип контейнера");
         }
     }
-    Stack tmp_stack(copyStack);
+    this->_pimpl = copyStack._pimpl;
+    /*Stack tmp_stack(copyStack);
     ValueType* tmp = new ValueType[tmp_stack.size()];
     size_t size_tmp = tmp_stack.size();
     for (int i = 0; i < size_tmp; ++i)
@@ -139,7 +140,7 @@ Stack& Stack::operator=(const Stack& copyStack)
         tmp_stack._pimpl->push(tmp[size_tmp - 1 - i]);
         this->_pimpl->push(tmp[size_tmp - 1 - i]);
     }
-    delete [] tmp;
+    delete [] tmp;*/
     return *this;
 }
 
